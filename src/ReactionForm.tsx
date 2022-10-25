b200224946fc5f97a1740a2c2d512e7f048a91fb
Gerard Keiser <gerardkeiser@gmail.com>
2022-10-25 17:57:11 -0500

    Stub for reaction form

--- src/ReactionForm.tsx
+++ src/ReactionForm.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { ChangeEvent, SyntheticEvent } from "react";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
+import "./ReactionForm.css";
 function ReactionForm({ idea, user }: ReactionFormProps): JSX.Element {
+  const submitInteresting = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const agreement = Number(e.target.value) - 4;
+  };
+
+  const submitBoring = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
-    <Box className="ReactionForm" component="form">
-      <TextField type="number" />
-    </Box>
+    <>
+      <Box className="ReactionForm" component="form">
+        <Typography component="legend">Agreement</Typography>
+        <Rating name="agreement" max={7} onChange={submitInteresting} />
+      </Box>
+      <Box className="ReactionForm" component="form" onSubmit={submitBoring}>
+        <Button type="submit">Boring</Button>
+      </Box>
+    </>
   );
 }
 
