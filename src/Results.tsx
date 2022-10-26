2fecccd572af54faa7cfe3ae766f0f2e218d89b4
Gerard Keiser <gerardkeiser@gmail.com>
2022-10-26 11:43:54 -0500

    Add graphs for reaction results

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,18 +1,39 @@
 import React from "react";
+import { BarChart, XAxis, Bar, Tooltip, Legend } from "recharts";
 
 const freqMap: (arr: Array<T>) => Map<T, number> = (arr) => {
-  return (sortedReactions = arr.reduce<Map<T, number>>((acc, elem) => {
+  return arr.reduce<Map<T, number>>((acc, elem) => {
     acc.has(elem) ? acc.set(elem, acc.get(elem) + 1) : acc.set(elem, 1);
     return acc;
-  }, new Map()));
+  }, new Map());
 };
+
+const dataArray = (map) =>
+  Array.from(map, ([name, value]) => ({ name, value }));
+
+const dataObj = (map: Map) => map;
 
 function Results({ results, reactions }): JSX.Element {
   console.log(results);
   console.log(reactions);
+  const graphReactions = dataArray(freqMap(reactions.reactions));
+  const graphAgreement = dataArray(freqMap(reactions.agreement));
+  console.log(graphAgreement);
+  console.log(graphReactions);
   return (
     <div className="Results">
-      <p>{freqMap(reactions.agreement)}</p>
+      <BarChart data={graphReactions} height={200} width={200}>
+        <XAxis dataKey="name" />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="value" fill="blue" />
+      </BarChart>
+      <BarChart data={graphAgreement} height={200} width={200}>
+        <XAxis dataKey="name" />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="value" fill="blue" />
+      </BarChart>
       <p>{freqMap(reactions.reactions)}</p>
     </div>
   );
