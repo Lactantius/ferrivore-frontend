fe19ebc9f7baa324ecc4fd19484fe23c52899ff5
Gerard Keiser <gerardkeiser@gmail.com>
2022-10-28 07:44:22 -0500

    Add styling to navbar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import LogoutIcon from "@mui/icons-material/Logout";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LightbulbIcon from "@mui/icons-material/Lightbulb";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 import "./NavBar.css";
 
@@ -9,12 +13,22 @@ function NavBar({ user, logout }: NavBarProps): JSX.Element {
       <NavLink to="/">Ferrivore</NavLink>
       {user ? (
         <>
-          <NavLink to="/profile">Profile</NavLink>
+          <NavLink to="/ideas" className="push">
+            <LightbulbIcon />
+            Ideas
+          </NavLink>
+          <NavLink to="/new">
+            <AddCircleOutlineIcon />
+            New
+          </NavLink>
+          <NavLink to="/profile">
+            <AccountCircleIcon />
+            Profile
+          </NavLink>
           <NavLink to="/" onClick={(e: React.MouseEvent) => logout()}>
+            <LogoutIcon />
             Logout
           </NavLink>
-          <NavLink to="/ideas">Ideas</NavLink>
-          <NavLink to="/new">New</NavLink>
         </>
       ) : (
         <>
